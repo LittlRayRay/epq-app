3b400254748bd5ffd67d494fd1a23096395d21f1
rayan nadeem <rayan.nadeem@outlook.com>
2024-08-27 22:32:21 +0100

    made it work kind of

--- project/main.py
+++ project/main.py
@@ -3,11 +3,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from flask import render_template
-
+from flask import redirect
+from flask import url_for
 from flask import Flask 
 from flask import Blueprint
+from flask import request
 import os
 import time
+import sqlite3
 
 
 main = Blueprint('main', __name__)
@@ -17,19 +20,70 @@ def dashboard():
 
     return render_template('dashboard.html')
 
+@main.route("/remove/<id>")
+def remove(id):
+    
+    con = sqlite3.connect("repairs.db")
+    cur = con.cursor()
+
+    cur.execute(f"DELETE FROM repairs WHERE primary_key = {id}") 
+    con.commit()
+    con.close()
+    return redirect(url_for('main.homepage'))
+
 
 @main.route("/")
 def homepage():
-    return render_template('homepage.html')
+    con = sqlite3.connect("repairs.db")
+    cur = con.cursor()
+    try:
+        res=cur.execute("SELECT primary_key, name FROM repairs;")
+        output = res.fetchall()
+    except:
+        print("table does not exist")
+        return "internal server error"
+    ouptut = [list([x[0], x[1]]) for x in output]
+    print(f"names of all projects: {output}")
+    con.close()
+    return render_template('homepage.html',projects=output)
 
-@main.route("/repair")
-def repair():
+@main.route("/make-new-repair")
+def new_repair():
+    return render_template('new_repair.html')
+
+@main.route("/add-new-repair", methods=['POST'])
+def add_new_repair():
+    name = request.form.get('name', "New Guide").strip()
+    url = request.form.get('url').strip().lower()
+    con = sqlite3.connect("repairs.db")
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS repairs(primary_key INTEGER PRIMARY KEY AUTOINCREMENT, name, url)")
+    cur.execute(f"INSERT INTO repairs (name, url) VALUES(?, ?);", (name, url))
+
+    con.commit()
+    con.close()
+
+    return redirect(url_for('main.homepage'))
+
+@main.route("/repair/<id>")
+def repair(id):
+    
+    return render_template('repair.html', id=id)
+
+
+@main.route("/intermed/<num>")
+def intermed(num):
+
+    con = sqlite3.connect("repairs.db")
+    cur = con.cursor()
+    res=cur.execute(f"SELECT url FROM repairs WHERE primary_key = {id};")
+    output=res.fetchall()
     driver = webdriver.Chrome()
-
-    driver.get("https://www.ifixit.com/Guide/iPhone+12+Screen+Replacement/140572")
-    
+    print(res)
+    driver.get(str(res))
     height = driver.execute_script("return document.body.scrollHeight")
-    
+
     # scrolls down the webpage to load images
     curr_height = 0
     while curr_height < (height):
@@ -39,7 +93,7 @@ def repair():
     elements = driver.find_elements(By.CLASS_NAME, "step") 
 
     return_string = ""
-    
+
     steps_texts=[]
     image_srcs=[]
 
@@ -50,9 +104,9 @@ def repair():
         os.remove("temp.txt")
     if os.path.exists("images.txt"):
         os.remove("images.txt")
-    
+
     steps_texts=list(dict.fromkeys(steps_texts))
-    
+
     images = driver.find_elements(By.TAG_NAME, 'img')
     image_srcs = [[] for i in range(len(steps_texts))]
     print(len(steps_texts))
@@ -64,11 +118,11 @@ def repair():
             print(step_no)
         else:
             print(image.get_attribute("id"))
-    
+
     with open("temp.txt", "w", encoding="utf-8") as text_file:
         for i in steps_texts:
             text_file.write(f"{i}\n")
-    
+
     with open("images.txt", "w", encoding="utf-8") as images_file: 
         for image in image_srcs:
             text_buff = ""
@@ -77,12 +131,12 @@ def repair():
                 text_buff+= f"{src.get_attribute('src')} "
             images_file.write(f"{text_buff}\n")
 
-    return render_template('index.html')
-
+    return redirect("/steps/0") 
 
 @main.route("/steps/<number>")
 def steps(number):
     text_print = ""
+    print(number)
     number = int(number)
     file = open("temp.txt")
     for n, text in enumerate(file):
